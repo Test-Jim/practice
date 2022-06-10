a3eac237a382de9acfc33a1f161d2ed1b84bf3d3
daozhang <305540786@qq.com>
2022-06-10 21:02:54 +0800

    面试题

--- test.py
+++ test.py
@@ -6,12 +6,12 @@ class seleniumTest(unittest.TestCase):
         self.s=requests.session()
         self.url='http://127.0.0.1:5000/get_baidu'
 
-    def test_geturl(self):
+    def test_geturl(self):#单元测试方法
         resp=self.s.get(url=self.url)
         secs=resp.content
         take_time=resp.elapsed.microseconds/1000000
-        self.assertTrue(take_time<1, msg='请求时间超过1s')
-        self.assertIs(secs,'//www.baidu.com/mg/bd_logo1.png','imageUrl error')
+        self.assertTrue(take_time<1, msg='请求时间超过1s')#断言时间是否超过1s
+        self.assertIs(secs,'//www.baidu.com/mg/bd_logo1.png','图片地址不一致')#断言返回是否一致
 
     @classmethod
     def tearDownClass(self):
